aa732940cee18f7e0da8016bfd6e52ed3692754c
ANIK-MOHANTA24 <mohontoa050@gmail.com>
2024-02-18 00:40:16 +0600

    added final score button

--- scripts/alpha_clash.js
+++ scripts/alpha_clash.js
@@ -12,6 +12,67 @@
 
 // ********************************
 
+//*****************for m-28.1********************* */
+
+function handelKeyboardButtonPress(event) {
+  const playerPressed = event.key;
+  // console.log('player pressed', playerPressed);
+
+  // get the expected to press
+  currentAlphabetElement = document.getElementById('current_alphabet');
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLowerCase();
+  // console.log(playerPressed, expectedAlphabet);
+
+  //check matched or not
+  if (playerPressed === expectedAlphabet) {
+    // console.log('you have pressed correctly', expectedAlphabet);
+    console.log('you get a point');
+
+    //1. get the current score
+    const currentScoreElement = document.getElementById('current-score');
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    // console.log(currentScore);
+
+    //2. increse the score by 1
+    const newScore = currentScore + 1;
+
+    //3. show the update score
+    currentScoreElement.innerText = newScore;
+
+    removeBackgroundColorById(expectedAlphabet);
+    continueGame();
+  } else {
+    //step-1. get the current life number
+    const currentLifeElement = document.getElementById('current_score');
+    const currentLifeText = currentLifeElement.innerText;
+    const currentLife = parseInt(currentLifeText);
+
+    //step-2. decrease the life number by 1
+    const newLife = currentLife - 1;
+
+    //step-3. show the update life number
+    currentLifeElement.innerText = newLife;
+
+    if (newLife === 0) {
+      // console.log('Game over');
+      gameOver();
+    }
+  }
+}
+
+document.addEventListener('keyup', handelKeyboardButtonPress);
+
+//********************************** */
+
+function gameOver() {
+  hideElementById('play_ground');
+  showElementById('final_score');
+}
+
+//******************************************** */
+
 function continueGame() {
   // stape-1: generate a random alphabet
   const alphabet = getARandomAlphabet();
@@ -26,7 +87,12 @@ function continueGame() {
 }
 
 function play() {
+  //hide everything & show only playground
   hideElementById('home_screen');
+  hideElementById('final_score');
   showElementById('play_ground');
+
+  //reset score and life
+
   continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -13,6 +13,23 @@ function setBackgroundColorById(elementId) {
   element.classList.add('bg-amber-600');
 }
 
+function removeBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove('bg-amber-600');
+}
+
+// ************m28.5****************
+function getTextElementValueById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValueText = element.innerText;
+  const value = parseInt(elementValueText);
+  return value;
+}
+function setTextElementValueById(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+
 function getARandomAlphabet() {
   const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
   const alphabets = alphabetString.split('');
